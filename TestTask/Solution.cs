e20930ef5a990f75655a2b303e385c3b6caaba52
nika <nvjagrdsohgjb@gmail.com>
2022-07-28 13:39:23 +0300

    Initial commit

---
+++ TestTask/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+
+namespace TestTask;
+
+public class Solution
+{
+    public string solution(string S, string T)
+    {
+        if (S.Equals(T))
+        {
+            return "EQUAL";
+        }
+        if (S.Length == T.Length)
+        {
+            var difference = S.Zip(T, (s, t) => (s, t)).Where(tupleItem => tupleItem.s != tupleItem.t).ToList();
+            if (difference.Count() == 1)
+            {
+                return $"REPLACE {difference.First().s} {difference.First().t}";
+            }
+            if (difference.Count() == 2)
+            {
+                if (difference.First().s == difference.Last().t && difference.First().t == difference.Last().s)
+                {
+                    return $"SWAP {difference.First().s} {difference.First().t}";
+                }
+            }
+        }
+        if (S.Length == (T.Length - 1))
+        {
+            for (int index = 0; index < S.Length; index++)
+            {
+                if (S[index] != T[index] && Enumerable.SequenceEqual(S.Skip(index), T.Skip(index + 1)))
+                {
+                    return $"INSERT {T[index]}";
+                }
+            }
+
+        }
+
+        return "IMPOSSIBLE";
+    }
+}
---
+++ TestTaskTests/SolutionTests.cs
@@ -0,0 +1,41 @@
+﻿using TestTask;
+namespace TestTaskTests;
+
+public class SolutionTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void EqualsTest()
+    {
+        var result = new Solution().solution("cat", "cat");
+        Assert.AreEqual(result, "EQUAL");
+    }
+    [Test]
+    public void ImpossibleTest()
+    {
+        var result = new Solution().solution("o", "odd");
+        Assert.AreEqual(result, "IMPOSSIBLE");
+    }
+    [Test]
+    public void InsertTest()
+    {
+        var result = new Solution().solution("nice", "niece");
+        Assert.AreEqual(result, "INSERT e");
+    }
+    [Test]
+    public void ReplaceTest()
+    {
+        var result = new Solution().solution("test", "tent");
+        Assert.AreEqual(result, "REPLACE s n");
+    }
+    [Test]
+    public void SwapTest()
+    {
+        var result = new Solution().solution("form", "from");
+        Assert.AreEqual(result, "SWAP o r");
+    }
+}
